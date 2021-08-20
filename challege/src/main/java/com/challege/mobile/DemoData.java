a2d68afb4764307ed7cf2c898b5a58bedf61c025
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-20 12:43:45 +0200

    Update DemoData.java

--- challege/src/main/java/com/challege/mobile/DemoData.java
+++ challege/src/main/java/com/challege/mobile/DemoData.java
@@ -5,68 +5,51 @@
  */
 package com.challege.mobile;
 
-
-import com.challege.mobile.repository.UserRepository;
-import com.challege.mobile.service.UserServiceImpl;
+import com.challege.mobile.service.UserService;
 import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 
 /**
  *
  * @author Andrea
  */
+
 @Component
 public class DemoData implements ApplicationRunner {
 
     @Autowired
-    UserServiceImpl userServiceImpl;
+    UserService userService;
     @Autowired
     CheckNumber checkNumber;
-
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         CSVReader reader;
         try {
-           InputStream in = new ClassPathResource("data/South_African_Mobile_Numbers.csv").getInputStream();
-   
-         
-           reader = new CSVReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-
+            InputStream in = new ClassPathResource("data/South_African_Mobile_Numbers.csv").getInputStream();
+            reader = new CSVReader(new InputStreamReader(in, StandardCharsets.UTF_8));
             String[] nextLine;
 
-            while ((nextLine = reader.readNext()) != null) {  
+            while ((nextLine = reader.readNext()) != null) {
                 try {
-                 
-                 userServiceImpl.saveUser(checkNumber.check(nextLine[0],nextLine[1]));
-                } catch (Exception ex) {ex.printStackTrace();}
-                        
-               
+                    userService.saveUser(checkNumber.check(nextLine[0], nextLine[1]));
+                } catch (Exception e) {
+                     System.err.println(e);
+                }
             }
-        
-        
+
         } catch (IOException e) {
-           e.printStackTrace();
+            System.err.println(e);
         }
         
-        
-
     }
+    
 }
