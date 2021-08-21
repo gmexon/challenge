f58bbde88d46d368d94064f2acff69c4401b3d64
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-21 16:42:15 +0200

    Update StatusStatisticsDTO.java

--- challege/src/main/java/com/challege/mobile/dto/StatusStatisticsDTO.java
+++ challege/src/main/java/com/challege/mobile/dto/StatusStatisticsDTO.java
@@ -1,1 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.challege.mobile.dto;
+
+/**
+ *
+ * @author Andrea
+ */
+public class StatusStatisticsDTO {
+  private String status;
+  private Long   cnt;
+
+    public StatusStatisticsDTO(String status, Long cnt) {
+        this.status = status;
+        this.cnt = cnt;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(Long cnt) {
+        this.cnt = cnt;
+    }
+  
+}
 
