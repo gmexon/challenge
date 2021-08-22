a4607070b2278000bb02c7b3e710be2901c502f9
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-22 13:45:14 +0200

    Update UserController.java

--- challege/src/main/java/com/challege/mobile/controller/UserController.java
+++ challege/src/main/java/com/challege/mobile/controller/UserController.java
@@ -8,11 +8,16 @@ import io.swagger.v3.oas.annotations.Parameter;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -79,4 +84,28 @@ public class UserController {
         }
         return new ResponseEntity<>(list, HttpStatus.OK);      
      }
+     
+     
+    @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> upload(@RequestPart("file") MultipartFile file) {
+        try {
+             userService.loadFile(file);
+            return ResponseEntity.status(HttpStatus.OK).body("OK");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("Error");
+        }
+    }
+    
+    @DeleteMapping("/allusers/")
+    public ResponseEntity<?> deleteAllUsers() {
+     try {
+             userService.deleteAllUsers();
+            return ResponseEntity.status(HttpStatus.OK).body("OK");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("Error");
+        }
+ }
+    
+    
 }
+
