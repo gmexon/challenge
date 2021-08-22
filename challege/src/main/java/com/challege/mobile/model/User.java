bfdaa96900ad49878781b2f53711a1ba2a62312f
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-22 03:08:40 +0200

    Update User.java

--- challege/src/main/java/com/challege/mobile/model/User.java
+++ challege/src/main/java/com/challege/mobile/model/User.java
@@ -1,12 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.challege.mobile.model;
 
 
 import com.challege.mobile.Status;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.io.Serializable;
 import javax.persistence.Column;
@@ -15,15 +11,18 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
 /**
  *
  * @author Andrea
  */
+
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private long id;
 
     @Column(name = "id_name")
@@ -90,8 +89,6 @@ public class User implements Serializable {
     public void setMessage(String message) {
         this.message = message;
     }
-    
-    
     
     
 }
