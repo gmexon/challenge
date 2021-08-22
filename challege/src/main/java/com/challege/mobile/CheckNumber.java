8a4ce0fda322ba350e0bc6e7ecff41183e20fc02
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-22 03:04:03 +0200

    Update CheckNumber.java

--- challege/src/main/java/com/challege/mobile/CheckNumber.java
+++ challege/src/main/java/com/challege/mobile/CheckNumber.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.challege.mobile;
 
 import com.challege.mobile.model.User;
@@ -13,19 +8,29 @@ import org.springframework.stereotype.Component;
  *
  * @author Andrea
  */
+
 @Component
 public class CheckNumber {
 
 
     public User check(String idName, String number) {
-
+        
         if (phoneNumberMatch(number, "^27\\d{9}$")) {
-            return new User(idName, number, Status.OK + "", "dont touch");
+            return new User(idName, number, Status.OK + "", "Not modified");
         }
         if (phoneNumberMatch(number, "\\d{9}$")) {
-            return new User(idName, "27" + number, Status.FIXED + "" + "", "add prefix 27");
+            return new User(idName, "27" + number, Status.FIXED + "" + "", "Add prefix 27");
         }
-        return new User(idName, number, Status.WRONG + "", "bad number");
+        if (!phoneNumberMatch(number, "^\\d*$")) {
+            return new User(idName, number, Status.WRONG + "" + "", "Not digit");
+        }
+        
+        if (phoneNumberMatch(number, "^\\d{1,8}$")) {
+            return new User(idName, number, Status.WRONG + "" + "", "Number of digits less than 9");
+        }
+        
+        
+        return new User(idName, number, Status.WRONG + "", "wrong number");
 
     }
 
