2502c3ad0004257c08e76da2b131b2f192c9d490
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-22 13:46:50 +0200

    Update UserService.java

--- challege/src/main/java/com/challege/mobile/service/UserService.java
+++ challege/src/main/java/com/challege/mobile/service/UserService.java
@@ -3,6 +3,7 @@ package com.challege.mobile.service;
 import com.challege.mobile.model.User;
 import com.challege.mobile.dto.StatusStatisticsDTO;
 import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -12,10 +13,12 @@ import java.util.List;
 public interface UserService {
     void saveUser(User user);
     User getUserById(long id);
-    void deleteUserById(long id);
+    void deleteAllUsers();
     List < User > getAllUsers();
     User getUserByIdName(String idName);
     List < User > findByStatusInUser(String status);
     List < StatusStatisticsDTO > getStatusCount();
-  
+    public void loadFile(MultipartFile file);
+
+    
 }
