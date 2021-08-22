e91ce3f2061218f238a909ef254675a33508700f
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-22 18:41:22 +0200

    Update DemoData.java

--- challege/src/main/java/com/challege/mobile/DemoData.java
+++ challege/src/main/java/com/challege/mobile/DemoData.java
@@ -1,11 +1,8 @@
 package com.challege.mobile;
 
 import com.challege.mobile.service.UserService;
-import com.opencsv.CSVReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,24 +26,12 @@ public class DemoData implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        CSVReader reader;
         try {
-            InputStream in = new ClassPathResource("data/South_African_Mobile_Numbers.csv").getInputStream();
-            reader = new CSVReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-            String[] nextLine;
-
-            while ((nextLine = reader.readNext()) != null) {
-                try {
-                    userService.saveUser(checkNumber.check(nextLine[0], nextLine[1]));
-                } catch (Exception ex) {
-                    Logger.getLogger(DemoData.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-
+            userService.loadFile(new ClassPathResource("data/South_African_Mobile_Numbers.csv").getInputStream());
         } catch (IOException ex) {
             Logger.getLogger(DemoData.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
 }
