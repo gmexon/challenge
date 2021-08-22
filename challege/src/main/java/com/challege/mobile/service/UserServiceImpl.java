618a8d53958f29fb8d623eb23d4eec392a81b6ba
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-22 13:48:03 +0200

    Update UserServiceImpl.java

--- challege/src/main/java/com/challege/mobile/service/UserServiceImpl.java
+++ challege/src/main/java/com/challege/mobile/service/UserServiceImpl.java
@@ -1,12 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.challege.mobile.service;
 
+import com.challege.mobile.CheckNumber;
 import com.challege.mobile.dto.StatusStatisticsDTO;
 import com.challege.mobile.model.User;
 import com.challege.mobile.repository.UserRepository;
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -14,10 +30,13 @@ import org.springframework.stereotype.Service;
  */
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
     
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    CheckNumber checkNumber;
 
     @Override
     public List<User> getAllUsers() {
@@ -26,6 +45,7 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
+    @Modifying
     public void saveUser(User user) {
     this.userRepository.save(user); 
     }
@@ -42,10 +62,6 @@ public class UserServiceImpl implements UserService {
         return user;
     }
 
-    @Override
-    public void deleteUserById(long id) {
-       this.userRepository.deleteById(id); 
-    }
 
     @Override
     public User getUserByIdName(String idName) {
@@ -69,6 +85,35 @@ public class UserServiceImpl implements UserService {
     public List<StatusStatisticsDTO> getStatusCount() {
        return userRepository.findStatusCount();
     }
+
+    @Override
+    @Modifying
+    public void loadFile(MultipartFile file) {
+        try {
+            CSVReader reader = new CSVReader(new InputStreamReader(file.getInputStream(), StandardCharsets.UTF_8));
+       
+            String[] nextLine;
+
+            while ((nextLine = reader.readNext()) != null) {
+                try {
+                    saveUser(checkNumber.check(nextLine[0], nextLine[1]));
+                } catch (Exception ex) {
+                      Logger.getLogger(UserServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        
+        } catch (CsvValidationException | IOException ex) {
+            Logger.getLogger(UserServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        
+        }
+    }
+
+    @Override
+    @Modifying
+    public void deleteAllUsers() {
+        userRepository.deleteAll();
+    }
+    
     
     
 }
