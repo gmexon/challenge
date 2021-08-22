92f1b5e41185865e964f0f6456e7edf5d6fe336f
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-22 03:14:03 +0200

    Update UserServiceImpl.java

--- challege/src/main/java/com/challege/mobile/service/UserServiceImpl.java
+++ challege/src/main/java/com/challege/mobile/service/UserServiceImpl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.challege.mobile.service;
 
 import com.challege.mobile.dto.StatusStatisticsDTO;
@@ -17,6 +12,7 @@ import org.springframework.stereotype.Service;
  *
  * @author Andrea
  */
+
 @Service
 public class UserServiceImpl implements UserService {
     
