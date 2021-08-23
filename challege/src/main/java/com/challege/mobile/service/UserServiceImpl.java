5b9e1097fe7cea1a638403a8c08572dea1625702
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-23 20:29:39 +0200

    Update UserServiceImpl.java

--- challege/src/main/java/com/challege/mobile/service/UserServiceImpl.java
+++ challege/src/main/java/com/challege/mobile/service/UserServiceImpl.java
@@ -14,10 +14,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
