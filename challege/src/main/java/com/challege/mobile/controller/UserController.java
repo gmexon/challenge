020b88b0caaa433490db6f4fd2c59aacfdd839ea
gmexon <89138839+gmexon@users.noreply.github.com>
2021-08-24 01:03:05 +0200

    Update UserController.java

--- challege/src/main/java/com/challege/mobile/controller/UserController.java
+++ challege/src/main/java/com/challege/mobile/controller/UserController.java
@@ -4,8 +4,12 @@ import com.challege.mobile.CheckNumber;
 import com.challege.mobile.dto.StatusStatisticsDTO;
 import com.challege.mobile.model.User;
 import com.challege.mobile.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import java.io.IOException;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +30,7 @@ import org.springframework.web.multipart.MultipartFile;
  * @author Andrea
  */
 @RestController
-@RequestMapping("/v1/api")
+@RequestMapping("/api")
 public class UserController {
 
     @Autowired
@@ -34,6 +38,9 @@ public class UserController {
     @Autowired
     CheckNumber checkNumber;
 
+    @Operation(summary = "Get user by IdName", responses = {
+    @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))),
+    @ApiResponse(responseCode = "404", description = "Not found", content = @Content) })
     @GetMapping("/user/{idName}")
     public ResponseEntity<User> user(@Parameter(description = "example: 103300640") @PathVariable("idName") String idName) {
         User user;
@@ -45,7 +52,10 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
 
     }
-
+    
+    @Operation(summary = "Get a list of all users", responses = {
+    @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = User.class)) )),   
+    @ApiResponse(responseCode = "500", description = "Error: Internal Server Error") })
     @GetMapping("/allusers/")
     public ResponseEntity<List<User>> users() {
         List<User> list;
@@ -56,7 +66,10 @@ public class UserController {
         }
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
-
+    
+    @Operation(summary = "Get a list of users filter by status", responses = {
+    @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = User.class)) )),    
+    @ApiResponse(responseCode = "500", description = "Error: Internal Server Error") })
     @GetMapping("/users/{status}")
     public ResponseEntity<List<User>> users(@Parameter(name = "status", schema = @Schema(type = "string", allowableValues = {"OK", "FIXED", "WRONG"})) @PathVariable("status") String stato) {
         List<User> list;
@@ -68,11 +81,22 @@ public class UserController {
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
+    
+    @Operation(summary = "Phone Number Validation", responses = {
+    @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = User.class))),
+    @ApiResponse(responseCode = "500", description = "Error: Internal Server Error") })
     @GetMapping("/check/{number}")
     public ResponseEntity<User> check(@Parameter(description = "example: 27730276061") @PathVariable("number") String number) {
+        try {
         return new ResponseEntity<>(checkNumber.check("check", number.trim()), HttpStatus.OK);
-    }
+         } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        }
 
+    @Operation(summary = "Counts all the records after the group by status", responses = {
+    @ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = StatusStatisticsDTO.class)) )),    
+    @ApiResponse(responseCode = "500", description = "Error: Internal Server Error") })
     @GetMapping("/statistics")
     public ResponseEntity<List<StatusStatisticsDTO>> statistics() {
         List<StatusStatisticsDTO> list;
@@ -84,16 +108,24 @@ public class UserController {
         return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
+    @Operation(summary = "Upload file CSV", responses = {
+    @ApiResponse(description = "Successful Operation", responseCode = "200" ),    
+    @ApiResponse(responseCode = "500", description = "Error: Internal Server Error"),
+    })
     @PostMapping(value = "/upload", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<?> upload(@RequestPart("file") MultipartFile file) {
+    public ResponseEntity<?> upload(@RequestPart("file") MultipartFile file)   {
         try {
-            userService.loadFile(file.getInputStream());
-            return ResponseEntity.status(HttpStatus.OK).body("OK");
-        } catch (IOException e) {
+          userService.loadFile(file.getInputStream());
+            return ResponseEntity.status(HttpStatus.OK).body("OK");  
+        } catch ( IOException e) {         
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body("Error");
         }
+     
     }
-
+    
+    @Operation(summary = "Delete all users", responses = {
+    @ApiResponse(description = "Successful Operation", responseCode = "200" ),    
+    @ApiResponse(responseCode = "500", description = "Error: Internal Server Error") })
     @DeleteMapping("/allusers/")
     public ResponseEntity<?> deleteAllUsers() {
         try {
